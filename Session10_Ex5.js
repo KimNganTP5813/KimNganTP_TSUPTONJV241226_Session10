3f6fb3c90a9520400502e709cf871d1ee9a8ca3e
KimNganTP5813 <akinosora13@gmail.com>
2025-02-18 22:35:40 +0900

    Add files via upload

---
+++ Session10_Ex5.js
@@ -0,0 +1,5 @@
+let firstNumber = prompt("Nhập vào số đầu tiên");
+let secondNumber = prompt("Nhập vào số thứ hai");
+
+//hiển thị bội số từ 2 số người dùng đã nhập
+alert(firstNumber % secondNumber);
