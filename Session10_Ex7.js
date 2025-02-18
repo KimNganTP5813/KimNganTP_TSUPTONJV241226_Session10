a945073a86f0020588f9a7c9f637ec10db8428ca
KimNganTP5813 <akinosora13@gmail.com>
2025-02-19 00:23:56 +0900

    Add files via upload

---
+++ Session10_Ex7.js
@@ -0,0 +1,9 @@
+let math = 9; //điểm môn toán
+let physics = 10; //điểm môn lý
+let chemistry = 10; //điểm môn hóa
+
+//tính tổng 3 môn
+let sum = math + physics + chemistry;
+
+//tính điểm trung bình 3 môn và hiển thị ra màn ảnh
+document.write("điểm trung bình là " + sum / 3);
