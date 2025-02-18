43160ba5acf157c59390012cd19168c36ea15ce4
KimNganTP5813 <akinosora13@gmail.com>
2025-02-18 22:19:21 +0900

    Add files via upload

---
+++ Session10_Ex3.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Rikkei Academy");
+
+document.write("Quyết tâm học lập trình Javascript");
+
+alert("Chào mừng bạn đến với học viện Rikkei Academy");
+
+const result = confirm("Bạn đã đủ 18 tuổi chưa!");
+alert(result);
