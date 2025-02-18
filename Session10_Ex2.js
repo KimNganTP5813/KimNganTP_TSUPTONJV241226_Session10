b560a5b8224270a183f9a6d3b1a8fcb72e3c2c8b
KimNganTP5813 <akinosora13@gmail.com>
2025-02-18 22:03:15 +0900

    Add files via upload

---
+++ Session10_Ex2.js
@@ -0,0 +1,9 @@
+let i = 10; //biến i kiểu số nguyên
+let f = 20.5; //biến f kiểu số thực
+let b = true; //biến b kiểu logic
+let s = "Hà Nội"; //biến s kiểu chuỗi
+
+document.write(i);
+document.write(f);
+document.write(b);
+document.write(s);
